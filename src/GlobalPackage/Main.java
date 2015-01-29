3a48e71108dc14ab77f2006a5e4c65813bcd21ae
Remi Somdecoste-Lespoune <saponace@gitbucket.org>
2015-01-29 23:58:59 +0100

    code allégé. Affiche maintenant dans la console le monde généré

--- src/GlobalPackage/Main.java
+++ src/GlobalPackage/Main.java
@@ -12,31 +12,22 @@ import com.jme3.app.SimpleApplication;
 //import com.jme3.scene.Node;
 //import com.jme3.scene.shape.Box;
  
-/** Sample 2 - How to use nodes as handles to manipulate objects in the scene.
- * You can rotate, translate, and scale objects by manipulating their parent nodes.
- * The Root Node is special: Only what is attached to the Root Node appears in the scene. */
 public class Main extends SimpleApplication {
 
 	
 	
+   World world = new World(10, 10, 10);
     
     public static void main(String[] args){
         Main app = new Main();
         app.start();
     }
  
-   World world = new World(1, 2, 3);
 
     @Override
     public void simpleInitApp() {
     	
+    world.printInConsole();
 
-        for(int i = 0; i < world.xSize(); i+=1){
-            for(int j = 0; j < world.ySize(); j+=1){
-            	for(int k = 0; k < world.zSize(); k+=1){
-            		System.out.printf(" " + world.getBlock(i, j, k));
-            	}
-            }
-        }
     }
 }
