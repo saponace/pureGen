2c78b3ae93c5c3edd30d1c8baccedb9465280827
Remi Somdecoste-Lespoune <saponace@gitbucket.org>
2015-01-29 23:58:31 +0100

    amélioration légère de l'affichage du monde dans la console

--- src/GlobalPackage/World.java
+++ src/GlobalPackage/World.java
@@ -17,8 +17,15 @@ public class World {
 
        for(int i = 0; i < x; i+=1)
             for(int j = 0; j < y; j+=1)
-            	for(int k = 0; k < z; k+=1)
-            		this.setBlock(i, j, k, BlockType.WATER);
+            	for(int k = 0; k < z; k+=1){
+            		if(j<6)
+            			this.setBlock(i, j, k, BlockType.STONE);
+            		else if(j<9)
+            			this.setBlock(i, j, k, BlockType.DIRT);
+            		else
+            			this.setBlock(i, j, k, BlockType.GRASS);
+
+            	}
 	}
 	
 	public BlockType getBlock(int x, int y, int z){
@@ -32,8 +39,30 @@ public class World {
 	public void printInConsole(){
 		for(int i = 0; i < this.xSize(); i+=1){
 			for(int j = 0; j < this.ySize(); j+=1){
-				for(int k = 0; k < this.zSize(); k+=1)
-					System.out.printf(this.getBlock(i, j, k) + " ");
+				for(int k = 0; k < this.zSize(); k+=1){
+					char charToPrint;
+					BlockType btype = this.getBlock(i, j, k);
+					switch(btype){
+					case AIR:
+						charToPrint = 'A';
+						break;
+					case GRASS:
+						charToPrint = 'G';
+						break;
+					case DIRT:
+						charToPrint = 'D';
+						break;
+					case STONE:
+						charToPrint = 'S';
+						break;
+					case WATER:
+						charToPrint = 'W';
+						break;
+					default:
+						charToPrint = 'X';
+					}
+					System.out.printf(charToPrint + " ");
+				}
 				System.out.printf("\n");
 			}
 			System.out.printf("\n");
