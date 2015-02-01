ba5494cb56ac5c19b272d4a1eb4b4acf32515b7c
Remi Somdecoste-Lespoune <saponace@gitbucket.org>
2015-02-01 01:29:17 +0100

    mise au propre pour la question sur le forul

--- src/GlobalPackage/Main.java
+++ src/GlobalPackage/Main.java
@@ -1,23 +1,17 @@
 package GlobalPackage;
 
-//import java.lang.reflect.Array;
-//
-//import jme3tools.optimize.GeometryBatchFactory;
 
 import com.jme3.app.SimpleApplication;
 //import com.jme3.material.Material;
-//import com.jme3.math.ColorRGBA;
+import com.jme3.math.ColorRGBA;
 //import com.jme3.math.Vector3f;
 //import com.jme3.scene.Geometry;
 //import com.jme3.scene.Node;
 //import com.jme3.scene.shape.Box;
- 
+
+
 public class Main extends SimpleApplication {
 
-	
-	
-   World world = new World(10, 10, 10);
-    
     public static void main(String[] args){
         Main app = new Main();
         app.start();
@@ -26,8 +20,11 @@ public class Main extends SimpleApplication {
 
     @Override
     public void simpleInitApp() {
-    	
-    world.printInConsole();
-
+		MeshedWorld.createMesh(0, 0, 0, MeshedWorld.Orientation.DOWN, new ColorRGBA(1, 1, 1, 1));
+		MeshedWorld.createMesh(0, 0, 0, MeshedWorld.Orientation.UP, new ColorRGBA(0, 0, 1, 1));
+		MeshedWorld.createMesh(0, 0, 0, MeshedWorld.Orientation.SOUTH, new ColorRGBA(0, 1, 1, 1));
+		MeshedWorld.createMesh(0, 0, 0, MeshedWorld.Orientation.WEST, new ColorRGBA(1, 1, 0, 1));
+		MeshedWorld.createMesh(0, 0, 0, MeshedWorld.Orientation.EAST, new ColorRGBA(1, 0, 0, 1));
+		MeshedWorld.createMesh(0, 0, 0, MeshedWorld.Orientation.NORTH, new ColorRGBA(1, 0, 0, 1));
     }
 }
