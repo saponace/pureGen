ab940299d59bd4b04b11becdfdee90e5d463210a
Remi Somdecoste-Lespoune <saponace@gitbucket.org>
2015-02-01 01:09:32 +0100

    changement de l'ordre des boucles utilisatnt les indices i, j et k pour respecter le modèle fixé

--- src/GlobalPackage/World.java
+++ src/GlobalPackage/World.java
@@ -22,9 +22,9 @@ public class World {
 	// Test constructor
 	public World(int x, int y, int z){
 		matrix = new BlockType[x][y][z];
-		for(int i = 0; i < x; i+=1)
-			for(int j = 0; j < y; j+=1)
-				for(int k = 0; k < z; k+=1){
+		for(int i = 0; i < x; i++)
+			for(int j = 0; j < y; j++)
+				for(int k = 0; k < z; k++){
 					if(j<6)
 						this.setBlock(i, j, k, BlockType.STONE);
 					else if(j<9)
@@ -55,11 +55,11 @@ public class World {
 
 
 	
-	// Display letters representing the world in te debug console
+	// Display letters representing the world in the debug console
 	public void printInConsole(){
-		for(int i = 0; i < this.xSize(); i+=1){
-			for(int j = 0; j < this.ySize(); j+=1){
-				for(int k = 0; k < this.zSize(); k+=1){
+		for(int i = 0; i < this.xSize(); i++){
+			for(int j = this.ySize()-1 ; j >= 0; j--){
+				for(int k = 0; k < this.zSize(); k++){
 					char charToPrint;
 					BlockType btype = this.getBlock(i, j, k);
 					switch(btype){
