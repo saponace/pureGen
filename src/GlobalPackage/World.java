5a35b925cc88e97ec312ddcec9b5eb581a1ab8a0
Remi Somdecoste-Lespoune <saponace@gitbucket.org>
2015-02-23 02:26:03 +0100

    début d'algo. our l'instant hauteur random puis lissage de la surface. Marche moyen moyen

--- src/GlobalPackage/World.java
+++ src/GlobalPackage/World.java
@@ -4,10 +4,10 @@ public class World {
 
 	// Enum of every blocktype
 	public static enum BlockType {AIR, GRASS, DIRT, STONE, WATER, OUT_OF_BOUNDS};
-	
+
 	// Matrix containing blocks. Core of the world
 	public static BlockType[][][] matrix;
-	
+
 	// Width of the world
 	public int xSize(){ return matrix.length;}
 
@@ -16,55 +16,92 @@ public class World {
 
 	// Height of the world
 	public int zSize(){ return matrix[0][1].length;} 
-	
+
+
+
+
+	// Smoothen the heightArray (lower height differences between close locations)
+	private int[][] smoothenSurface(int[][] heightArray, int radius, int iterations){
+		int maxx = heightArray.length;
+		int maxz = heightArray[0].length;
+		for(int count = 0; count < iterations; count++)
+			for(int i= 0; i < maxx; i++)
+				for(int k= 0; k < maxz; k++){
+					int coef = 0;
+					for(int di=radius*-1; di<radius; di++)
+						for(int dk=radius*-1; dk<radius; dk++){
+							if(i+di > 0 && i+di < maxx && k+dk > 0 && k+dk < maxz){
+								if(heightArray[i][k] < heightArray[i+di][k+dk])
+									coef++;
+								else if(heightArray[i][k] > heightArray[i+di][k+dk])
+									coef--;
+							}
+						}
+					if(coef > 0)
+						heightArray[i][k]++;
+					else if(coef < 0)
+						heightArray[i][k]--;
+				}
+		return heightArray;
+
+	}
+
+
+
+	// Create the array giving the height of the surface of the world
+	private int[][] createHeightArray(int xmin, int xmax, int ymin, int ymax, int zmin, int zmax){
+		int[][] heightArray = new int[xmax-xmin][zmax-zmin];
+
+		// Première passe, complètement random
+		for(int i=xmin; i < xmax; i++)
+			for(int k=zmin; k < zmax; k++){
+				int height = 0;
+				while(height < ymin)
+					height = (int)(Math.random()*ymax);
+				heightArray[i][k] = height;
+			}
+		smoothenSurface(heightArray, 10, 3);
+		return heightArray;
+	}
 
 
 	// Test constructor
 	public World(int x, int y, int z){
 		matrix = new BlockType[x][y][z];
+		int[][] heightArray = createHeightArray(0, x, 1, y, 0, z); 
 		for(int i = 0; i < x; i++)
-			for(int j = 0; j < y; j++)
-				for(int k = 0; k < z; k++){
-					if(j<3)
-						this.setBlock(i, j, k, BlockType.STONE);
-					else if(j<5)
-						this.setBlock(i, j, k, BlockType.DIRT);
-					else if(j<6)
+			for(int k = 0; k < z; k++){
+				for(int j = 0; j < y; j++){
+					if(j < heightArray[i][k]){
 						this.setBlock(i, j, k, BlockType.GRASS);
-					else if(j == 6 && i%5==0 && k%5==0)
-						this.setBlock(i, j, k, BlockType.GRASS);
-					else
+						//						System.out.printf("%d, %d\n", j, height);
+					}
+					else 
 						this.setBlock(i, j, k, BlockType.AIR);
 				}
-//		this.setBlock(5, 5, 5, BlockType.WATER);
-//		this.setBlock(5, 5, 6, BlockType.WATER);
-//		this.setBlock(6, 5, 5, BlockType.WATER);
-//		this.setBlock(6, 5, 6, BlockType.WATER);
-//		this.setBlock(5, 5, 7, BlockType.WATER);
-//		this.setBlock(6, 5, 7, BlockType.WATER);
-//		this.setBlock(5, 4, 5, BlockType.WATER);
+			}
 	}
-	
-	
-	
+
+
+
 	// Get a block on the given coordinates
 	public BlockType getBlock(int x, int y, int z){
 		if(x < 0 || x >= this.xSize() ||
-		   y < 0 || y >= this.ySize() ||
-	       z < 0 || z >= this.zSize())
+				y < 0 || y >= this.ySize() ||
+				z < 0 || z >= this.zSize())
 			return BlockType.OUT_OF_BOUNDS;
 		else return matrix[x][y][z];
 	}
 
 
-	
+
 	// Set a block on the given coordinates
 	public void setBlock(int x, int y, int z, BlockType block){
 		matrix[x][y][z] = block;
 	}
 
 
-	
+
 	// Display letters representing the world in the debug console
 	public void printInConsole(){
 		for(int i = 0; i < this.xSize(); i++){
